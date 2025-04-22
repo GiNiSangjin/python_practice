de273bcd233013aa3d97754b86d57581b79456a7
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-22 16:50:40 +0900

    연습문제

---
+++ day04/1.py
@@ -0,0 +1,11 @@
+#password = input('비밀번호 입력 : ')
+password="123123123"
+print(len(password))
+print(len(password)>=6)
+print(len(password)<=12)
+
+#비교연산자
+number = 7
+cnt = 5
+
+print(number ** 2 < cnt %2 + 5)
