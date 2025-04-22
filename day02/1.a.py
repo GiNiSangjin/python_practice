521baaec630e356f900fe573aa30db07b14a9191
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-22 11:53:14 +0900

    문자열 만들기

---
+++ day02/1.a.py
@@ -0,0 +1,17 @@
+#문자열 만들기
+sentnece ='''내가 말했다. \n"빨리 출발해"'''
+print(sentnece)
+
+#3개의 단일 부호로 문자열을 만들 수 있다. 
+poem = '''So "it is" quite different, then, if in a mountain town
+the mountains are close, rather than far. Here
+
+they are far, their distance away established,
+consistent year to year, like a parent’s
+
+or sibling’s. They have their own music.
+So I confess I do not know what it’s like,
+
+listening to mountains up close, like a lover,
+the silence of them known, not guessed at.'''
+print(poem)
