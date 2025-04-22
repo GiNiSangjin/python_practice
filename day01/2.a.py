2e0a2e7cde2288e1bdcc3f95f410c2c2031cb5cd
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-22 11:14:37 +0900

    print, input, 형변환

---
+++ day01/2.a.py
@@ -0,0 +1,19 @@
+#print함수
+sentence="안녕하세요"
+print(sentence)
+
+name="홍길동"
+age=55
+print('이름은 {}이고 나이는 {}세입니다.'.format(name, age))
+
+#user input
+name=input('이름을 입력하세요: ')
+age=input('나이를 입력하세요: ')
+print('이름은 {}이고 나이는 {}세입니다.'.format(name, age))
+
+#형변환
+number1 = input('첫 번째 숫자를 입력하세요: ')
+number2 = input('두 번째 숫자를 입력하세요: ')
+number1 = int(number1)
+number2 = int(number2)
+print('두 숫자의 합은 {}입니다.'.format(number1 + number2))
