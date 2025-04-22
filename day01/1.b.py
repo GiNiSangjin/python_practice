1e530e47251e5807e453b30967c49225bb549e48
Sangjin Jeong <sangjinjeong@Sangjinui-MacBookPro.local>
2025-04-22 11:09:59 +0900

    예제 풀이

---
+++ day01/1.b.py
@@ -0,0 +1,33 @@
+#넓이와 둘레길이를 구하는 코드를 만드세요. a=11. b=7일 때의 넓이와 둘레의 길이
+a=11
+b=7
+area=a*b
+perimeter=2*(a+b)
+print('area:', area)
+print('perimeter:', perimeter)
+
+#어떤 숫자 x가 주어졌을 때, 그 숫자가 짝수인지 홀수인지 계산하는 코드를 만드세요.
+x=15123
+if x%2==0:
+    print('짝수')
+else:
+    print('홀수')
+
+#a의 길이가 20으로 주어졌을 때, area와 volume를 구하는 코드를 만드세요.
+a=20
+area=6*a**2
+volume=a**3
+print('area:', area)
+print('volume:', volume)
+
+#x가 4, y가 2일 때 다음 식을 만족하는 z를 구하는 코드를 만드세요.
+x=4
+y=2
+z=abs(x-y) * (x+y)
+print('z:', z)
+
+#현재 속도가 90km/h일 때, mp/h를 계산하는 코드를 만드세요.
+kmh=90
+mph=kmh/1.609
+print('mph:', mph)
+
